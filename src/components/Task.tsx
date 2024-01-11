e63c7eecfc9cc31a06741ba0f9e57b128368c485
Davi J <davijosantos.dj@gmail.com>
2024-01-11 13:33:29 -0300

    Design checked in tasks done

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,19 +1,26 @@
 import { FiTrash2 } from "react-icons/fi";
 import styles from "./Task.module.css";
+import { useState } from "react";
 
 export interface TaskProps {
   id: number;
   content: string;
-  checked: boolean;
 }
 
-export function Task({ content, checked }: TaskProps){
+export function Task({ content }: TaskProps){
+  const [check, setCheck] = useState(false)
+
+  function handleClickCheck() {
+    setCheck(!check);
+  }
+
   return (
     <li className={styles.taskItem}>
-      <div>
-        <input type="radio" checked={checked} />
-      </div>
-      <p>
+      <label>
+        <input className={styles.checkBox} type="checkbox" onClick={handleClickCheck} checked={check}/>
+        <span className={check ? styles.checkmark : ''}></span>
+      </label>
+      <p className={check ? styles.contentCheck : styles.contentNotCheck} >
         {content}
       </p>
       <button className={styles.deleteButton}>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,56 +1,51 @@
 import { Task, TaskProps } from "./Task";
 import { LuClipboardList  } from "react-icons/lu";
 import styles from "./TaskList.module.css";
+import { useState } from "react";
 
-const tasks: TaskProps[] = [
-  {
-    id: 0,
-    content: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    checked: false,
-  },
-  {
-    id: 1,
-    content: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    checked: false,
-  },
-  {
-    id: 2,
-    content: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    checked: false,
-  },
-]
 
 export function TaskList(){
- return (
-  <section>
-    <div className={styles.info}>
-      <div className={styles.createdTasks}>
-        Tarefas criadas
-        <span>0</span>
-      </div>
-      <div className={styles.tasksDone}>
-        Concluídas
-        <span>0</span>
-      </div>
-    </div>
+  const [tasks, setTasks] = useState<TaskProps[]>([
+    {
+      id: 0,
+      content: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    },
+    {
+      id: 1,
+      content: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    },
+  ])
 
-    <ul className={styles.listTasks}>
-      {
-        tasks.length === 0 ? 
-        <p className={styles.empty}>
-          <LuClipboardList size={52} />
-          <strong>Você ainda não tem tarefas cadastradas</strong>
-          <span>Crie tarefas e organize seus itens a fazer</span>
-        </p>
-        :
-        tasks.map((task) => {
-          return (
-            <Task key={task.id} content={task.content} checked={task.checked} id={task.id} />
-          )
-        })
-      }
-    </ul>
+  return (
+    <section>
+      <div className={styles.info}>
+        <div className={styles.createdTasks}>
+          Tarefas criadas
+          <span>0</span>
+        </div>
+        <div className={styles.tasksDone}>
+          Concluídas
+          <span>0</span>
+        </div>
+      </div>
 
-  </section>
+      <ul className={styles.listTasks}>
+        {
+          tasks.length === 0 ? 
+          <p className={styles.empty}>
+            <LuClipboardList size={52} />
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <span>Crie tarefas e organize seus itens a fazer</span>
+          </p>
+          :
+          tasks.map((task) => {
+            return (
+              <Task key={task.id} content={task.content} id={task.id} />
+            )
+          })
+        }
+      </ul>
+
+    </section>
  ); 
 }
